0fb15daaa6507a766b363b7d2d28e5f5304e5ea5
Uriel Miranda Miñon <umminon@gmail.com>
2019-03-29 20:59:38 -0600

    Parte1.sh

--- Parte1.sh
+++ Parte1.sh
@@ -1,7 +1,7 @@
 # bin/bash
 
 ## Encoding
-iconv -f utf8 -t ascii//TRANSLIT all_data.csv > profeco_sin_acentos.csv
+iconv -f utf8 -t ascii//TRANSLIT data/prueba.csv > profeco_sin_acentos.csv
 
 ## Remove "." andRemove "." and  Convert to lowercase 
 awk '{gsub(/\./, "", $0); print tolower($0)}' profeco_sin_acentos.csv  > profeco_minusculas.csv
@@ -12,5 +12,5 @@ awk -F'"' '{ for (i=1; i<=NF; i+=2) gsub(",", "|", $i) } 1' profeco_minusculas.c
 
 ## Validate number of fields by row
 
-awk -F'|' '{print NF}' profeco_final_bash.csv | uniq
+awk -F'|' '{ NF}' profeco_final_bash.csv | uniq
 
