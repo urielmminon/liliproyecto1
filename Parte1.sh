dc28e6cf7dfbecc0987d1d3c06bb430135a2a434
Uriel Miranda Miñon <umminon@gmail.com>
2019-03-29 20:10:26 -0600

    Parte 1 final (toda la base)

--- Parte1.sh
+++ Parte1.sh
@@ -1,7 +1,7 @@
 # bin/bash
 
 ## Encoding
-iconv -f utf8 -t ascii//TRANSLIT data/prueba.csv > profeco_sin_acentos.csv
+iconv -f utf8 -t ascii//TRANSLIT all_data.csv > profeco_sin_acentos.csv
 
 ## Remove "." andRemove "." and  Convert to lowercase 
 awk '{gsub(/\./, "", $0); print tolower($0)}' profeco_sin_acentos.csv  > profeco_minusculas.csv
@@ -9,3 +9,8 @@ awk '{gsub(/\./, "", $0); print tolower($0)}' profeco_sin_acentos.csv  > profeco
 ## Substitute inter "," for "|" and remove "
 
 awk -F'"' '{ for (i=1; i<=NF; i+=2) gsub(",", "|", $i) } 1' profeco_minusculas.csv > profeco_final_bash.csv
+
+## Validate number of fields by row
+
+awk -F'|' '{print NF}' profeco_final_bash.csv | uniq
+
