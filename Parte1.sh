be81f9ce823efff1a03b71ba4ab3203e25fcf7c1
Uriel Miranda Miñon <umminon@gmail.com>
2019-03-29 19:39:06 -0600

    Subs. , for pipe and remove

--- Parte1.sh
+++ Parte1.sh
@@ -6,4 +6,6 @@ iconv -f utf8 -t ascii//TRANSLIT data/prueba.csv > profeco_sin_acentos.csv
 ## Remove "." andRemove "." and  Convert to lowercase 
 awk '{gsub(/\./, "", $0); print tolower($0)}' profeco_sin_acentos.csv  > profeco_minusculas.csv
 
+## Substitute inter "," for "|" and remove "
 
+awk -F'"' '{ for (i=1; i<=NF; i+=2) gsub(",", "|", $i) } 1' profeco_minusculas.csv > profeco_final_bash.cs > profeco_final_bash.csvv
