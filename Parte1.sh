1b1d9d95aad1ba7dde1afcba1d22edac4bafd892
Uriel Miranda Miñon <umminon@gmail.com>
2019-03-29 19:33:08 -0600

    remove . and convert to lowercase

--- Parte1.sh
+++ Parte1.sh
@@ -3,5 +3,7 @@
 ## Encoding
 iconv -f utf8 -t ascii//TRANSLIT data/prueba.csv > profeco_sin_acentos.csv
 
-## Convert to lowercase
-awk '{print tolower($0)}' profeco_sin_acentos.csv > profeco_minusculas.csv
+## Remove "." andRemove "." and  Convert to lowercase 
+awk '{gsub(/\./, "", $0); print tolower($0)}' profeco_sin_acentos.csv  > profeco_minusculas.csv
+
+
