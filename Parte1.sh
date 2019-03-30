01f1d8cbdfaf253e4ba602d55eac0817dc73cdf7
Uriel Miranda Miñon <umminon@gmail.com>
2019-03-29 19:20:17 -0600

    To lower

--- Parte1.sh
+++ Parte1.sh
@@ -1,3 +1,7 @@
 # bin/bash
 
-iconv -f utf8 -t ascii//TRANSLIT data/prueba.csv
+## Encoding
+iconv -f utf8 -t ascii//TRANSLIT data/prueba.csv > profeco_sin_acentos.csv
+
+## Convert to lowercase
+awk '{print tolower($0)}' profeco_sin_acentos.csv > profeco_minusculas.csv
